3ce88daf420ca061bbbdd8a1c2abb4e121a0ca44
Tom Sherman <the.tomsherman@gmail.com>
2023-03-01 00:36:26 +0000

    Move to a param

--- app/routes/$id.tsx
+++ app/routes/$id.tsx
@@ -6,15 +6,17 @@ const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 export function loader() {
   return defer({
+    done: Math.random(),
     deferredRandom: wait(2000).then(() => Math.random()),
   });
 }
 
 export default function Index() {
-  const { deferredRandom } = useLoaderData<typeof loader>();
+  const { deferredRandom, done } = useLoaderData<typeof loader>();
   return (
     <>
-      <Link to="/">Reload</Link>
+      <Link to={`/${done}`}>Reload</Link>
+      <div>Done: {done}</div>
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={deferredRandom}>
           {(random) => <div>Random: {random}</div>}
